11770b81bda19fb633049c0707259f48490542db
Sourcery AI <>
2023-06-23 09:55:26 +0000

    'Refactored by Sourcery'

--- src/logic.py
+++ src/logic.py
@@ -3,15 +3,15 @@ import os
 
 prefix = 'unset LD_PRELOAD; unset LD_LIBRARY_PATH; /system/bin/bootctl '
 
-stream = os.popen(prefix + 'hal-info')
+stream = os.popen(f'{prefix}hal-info')
 output = stream.read()
 pyotherside.send('halVersion', output)
 
-stream = os.popen(prefix + 'get-number-slots')
+stream = os.popen(f'{prefix}get-number-slots')
 output = stream.read()
 pyotherside.send('noOfSlots', output)
 
-stream = os.popen(prefix + 'get-current-slot')
+stream = os.popen(f'{prefix}get-current-slot')
 output = stream.read()
 temp = (int(output))
 
@@ -21,12 +21,12 @@ if (temp == 0):
 elif (temp == 1):
     pyotherside.send('currentSlot', "B")
 
-stream = os.popen(prefix + 'get-suffix ' + str(temp))
+stream = os.popen(f'{prefix}get-suffix {temp}')
 output = stream.read()
 pyotherside.send('currentSlotSuffix', output)
 
 def switchSlotFunc():
-    if (temp == 0):
-        os.system(prefix + 'set-active-boot-slot 1 && sudo reboot') # will only reboot if no password is set on device
-    elif (temp == 1):
-        os.system(prefix + 'set-active-boot-slot 0 && sudo reboot')
+    if temp == 0:
+        os.system(f'{prefix}set-active-boot-slot 1 && sudo reboot')
+    elif temp == 1:
+        os.system(f'{prefix}set-active-boot-slot 0 && sudo reboot')
